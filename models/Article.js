2a2114cbfbe050e18b720fe5b5fdf23a365a535e
acquahsamuel <acquahsamuel1996@gmail.com>
2021-08-25 12:45:45 +0000

    [WIP]+> models: articles.js

--- models/Article.js
+++ models/Article.js
@@ -2,72 +2,73 @@ const mongoose = require("mongoose");
 const slugify = require("slugify");
 
 const ArticleSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'User',
-    //FIXME: @dd required : true
+  title: {
+    type: String,
+    required: true,
+  },
+
+  subTitle: {
+    type: String,
+  },
+
+  content: {
+    type: String,
+    default: true,
   },
 
   url: String,
 
   category: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'Category',
-    //FIXME: @dd required : true
+    type: String,
+    enum: ["bussiness", "politics", "entertainment", "sports", "general"],
+    default: "general",
   },
 
   language: {
     type: String,
-    enum: ['en', 'tr', 'es'],
-    default: 'en'
+    enum: ["en", "tr", "es"],
+    default: "en",
   },
-  title: {
-    type: String,
-    required: true
-  },
-  subTitle: {
-    type: String,
-  },
+
   status: {
     type: String,
-    enum: ['private', 'public'],
-    default: "public"
+    enum: ["private", "public"],
+    default: "public",
   },
 
   allowComments: {
     type: Boolean,
-    required: true
-  },
-
-  content: {
-    type: String,
-    default: true
+    required: true,
   },
 
   image: {
-    type: String
+    type: String,
   },
 
-  date: {
+  createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
+  },
+
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    //FIXME: @dd required : true
   },
 
   //FIXME: { if array of just object }
   comments: {
     type: mongoose.Schema.ObjectId,
-    ref: "Comment"
+    ref: "Comment",
     //FIXME: @dd required : true
-  }
+  },
 });
-
 
 ArticleSchema.pre("save", function (next) {
   this.url = slugify(this.title, {
-    lower: true
+    lower: true,
   });
   next();
 });
-
 
 module.exports = mongoose.model("Article", ArticleSchema);
