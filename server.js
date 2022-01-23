5c37df239430e7ae8f66396ed1b793fe97e6531f
acquahsamuel <iamsamuelacquah@gmail.com>
2022-01-23 22:44:58 +0000

    server.js

--- server.js
+++ server.js
@@ -68,24 +68,6 @@ app.use(hpp());
 // Enable CORS
 app.use(cors());
 
-// Set CORS for different client access
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST , PUT , PATCH , DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Header", "Content-Type,  Authorization");
-  next();
-});
-
-// Enable ejs as view engine
-app.set("view engine", "ejs");
-app.set("views", "views");
-
-// Set static folder
-app.use(express.static(path.join(__dirname, "public")));
-
 // Mount routers
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/users", user);
@@ -98,9 +80,7 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 const server = app.listen(
   PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-  )
+  console.log(`Server running on port ${PORT}`.yellow.bold)
 );
 
 // Handle unhandled promise rejections
